8549121b502660b1e81751d651811039b07336bb
sairam2098 <dvvsairam123@gmail.com>
2020-11-21 16:39:43 +0530

    Added graphql setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Book } from "./entities/book";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
-    // const book = orm.em.create(Book, {title: 'The Alchemist'});
-    // await orm.em.persistAndFlush(book);
+    const app = express();
 
-    const books = await orm.em.find(Book, {});
-    console.log(books);
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("server listning on port 4000")
+    });
 }
 
 main().catch(err => {
